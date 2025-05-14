5a2a9990f9563eb31d99c24f25aee6b9991146b8
syedmin84 <syedmin84@gmail.com>
2025-05-14 18:46:39 +0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main className="flex justify-center items-center h-screen">
+        <section className="mx-auto w-[900px] h-[500px] bg-[#000] shadow-[0_0_50px_#0ef] rounded-[20px] p-[20px] overflow-hidden"></section>
+      </main>
+    </>
+  );
+}
+
+export default App;
